789e0092c94aa7fd638a532492e034df099b159d
Влад Пасечник <jump.e.r@yandex.ru>
2022-03-25 17:31:28 +0300

    first

--- packages/cas/ios/Classes/CleverAdsSolutions.swift
+++ packages/cas/ios/Classes/CleverAdsSolutions.swift
@@ -54,38 +54,3 @@ public class CleverAdsSolutions {
         return CAS.validateIntegration()
     }
 }
-
-public class AdContentDelegate: CASCallback {
-    let placement: String
-    let channel: FlutterMethodChannel
-
-    init(placement: String, channel: FlutterMethodChannel) {
-        self.placement = placement
-        self.channel = channel
-    }
-
-    public func willShown(adStatus ad: CASImpression) {
-        print("TEST TEST")
-        channel.invokeMethod("onShown", arguments: [placement, ad.adType.toPrefix(), ad.network, ad.priceAccuracy, ad.cpm, ad.status, ad.error, ad.versionInfo, ad.identifier])
-    }
-
-    public func didShowAdFailed(error: String) {
-        print("TEST TEST")
-        channel.invokeMethod("onShowFailed", arguments: [placement, error])
-    }
-
-    public func didClickedAd() {
-        print("TEST TEST")
-        channel.invokeMethod("onClicked", arguments: placement)
-    }
-
-    public func didCompletedAd() {
-        print("TEST TEST")
-        channel.invokeMethod("onComplete", arguments: placement)
-    }
-
-    public func didClosedAd() {
-        print("TEST TEST")
-        channel.invokeMethod("onClosed", arguments: placement)
-    }
-}
